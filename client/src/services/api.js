b3c584c866bf09c3f506a740567fd130fb12d410
João Pedro Lima Pirajá <jplp100@hotmail.com>
2022-11-20 11:06:54 -0300

    minor fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://poc-enviar-arquivos.onrender.com/',
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://poc-enviar-arquivos.onrender.com/',
+  // baseURL: 'http://localhost:3333',
 });
 
 export default api;
